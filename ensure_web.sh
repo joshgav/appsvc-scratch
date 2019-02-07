896dbd98b5b80ec38860db497446ea22612dcf74
Josh Gavant <joshgavant@gmail.com>
2019-02-07 10:12:53 -0800

    add env var handling

--- ensure_web.sh
+++ ensure_web.sh
@@ -10,6 +10,10 @@ source "${__dirname}/util/helpers.sh"
 export -f ensure_group  # from rm_helpers.sh, make available to children
 ## end prolog
 
+### $WEB_ENV_VARS should be an indexed array variable
+#   each item in the array should be a `key=value` pair
+declare -a env_vars=${WEB_ENV_VARS}
+
 az account set --subscription $SUBSCRIPTION_NAME
 
 group_id=$(ensure_group $GROUP_NAME $GROUP_LOCATION)
@@ -19,7 +23,7 @@ echo "ensured group [${group_id}]"
 plan_id=$(az appservice plan show \
   --name $PLAN_NAME \
   --resource-group $GROUP_NAME \
-  --query 'id' --output 'tsv')
+  --query 'id' --output 'tsv' || true)
 if [[ -z $plan_id ]]; then
   plan_id=$(az appservice plan create \
               --name $PLAN_NAME \
@@ -35,7 +39,7 @@ echo "ensured plan [${plan_id}]"
 web_id=$(az webapp show \
   --name $WEB_NAME \
   --resource-group $GROUP_NAME \
-  --query 'id' --output tsv)
+  --query 'id' --output tsv || true)
 if [[ -z $web_id ]]; then
   web_id=$(az webapp create \
             --name $WEB_NAME \
@@ -52,6 +56,14 @@ az webapp log config \
   --docker-container-logging filesystem \
   --level information 1> /dev/null
 echo "enabled logging for web [${web_id}]"
+
+### set environment variables
+if [[ -n "${env_vars[@]}" ]]; then
+  >&2 echo "setting env vars in cloud"
+  az webapp config appsettings set \
+      --id ${web_id} \
+      --settings $env_vars
+fi
 
 ### switch to Oryx builder
 # az webapp config appsettings set \
